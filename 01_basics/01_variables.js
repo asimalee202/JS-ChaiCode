8e67516fea9d6698ada681db952931f21da11e8d
asimalee202 <125573648+asimalee202@users.noreply.github.com>
2025-02-07 17:14:03 +0000

    hope

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "ali@gmail.com"
+var accountPassword = "abcd"
+accountCity = "Peshawari"
+
+// accountId = 11
+console.log(accountId, accountEmail)
+console.table([accountId, accountEmail, accountPassword, accountCity]);
